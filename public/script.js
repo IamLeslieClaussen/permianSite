74ae1b4d21f57c8533dd631415008d480a119ce4
Leslie Claussen <78480772+IamLeslieClaussen@users.noreply.github.com>
2025-11-28 15:24:55 +0000

    latest commit

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
-import { gsap, selector } from "gsap";
-import { SplitText } from "./SplitText.min.js";
-import { type } from "os";
+import { gsap } from "gsap";
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
   gsap.registerPlugin(SplitText);
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const splitTextIntoLines = (selector, options = {}) => {
     const defaults = {
       type: "lines",
-      mask: "lines",
+      mask: "line",
       lineClass: " line",
       ...options,
     };
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if(currentValue < 100 && elapsedTime < maxDuration) {
           const target = Math.floor(progress * 100);
           const jump = Math.floor(Math.random() * 25) + 5;
-          currentValue = Math.min(currentValue * jump, target, 100);
+          currentValue = Math.min(currentValue + jump, target, 100);
 
           counterElement.textContent = currentValue.toString().padStart(2, "0");
           setTimeout(updateCounter, updateInterval + Math.random() * 100)
@@ -51,6 +51,17 @@ document.addEventListener("DOMContentLoaded", () => {
     }, delay * 1000);
   }
      
+  animateCounter(".preloader-counter p", 4.5, 2);
 
+  const tl = gsap.timeline();
+
+  tl.to([".preloader-copy p .line", ".preloader-counter p .line"], {
+    y: "0%",
+    duration: 1,
+    stagger: 0.075,
+    ease: "power3.out",
+    delay: 1,
+  });
+  
     
 });
